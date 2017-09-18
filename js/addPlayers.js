31b865f97f7b6e067fce346e9f80dfa7b6e9c62b
Ben Naylor <bennaylor@Bens-MacBook-Air.local>
2017-09-18 11:42:46 +0100

    progress pushing winner and dsiplaying next round

--- js/addPlayers.js
+++ js/addPlayers.js
@@ -5,60 +5,25 @@ $(document).ready($ => {
 	let list = $(".list"); //pre shuffle list of names 
 	let startGame = $("#start"); 
 	let store = []; // intial array
-	let winners=[]; // next round array 
+	 // next round array 
 	let display = $("#display"); 
 	let error =$("#error");
 	let round= $("#round");
-	let winner =$("#winner")
+	
+	let test =$("#test");
 	counter =0;
 
-	let shufflePlayers =() =>{
-		
-		store.sort((a, b)=> 0.5 - Math.random());
-		store.map((val,index) => {
-
-		    if (index % 2 === 0 || index === 0){
-		    	counter += 1;
-		    	
-		    	// Games push to the index page, game number, the players names and 
-		    	//radio buttons to record the win. 
-		    	let games ="<h3>"+"Game"+" "+counter+"</h3>"+
-	    	 		'<input type="radio" name='+store[index]+' value="'+store[index]+'"><label>'+store[index]+'</label>'+" "+ "v"+ " " +
-	    	 		'<input type="radio" name='+store[index]+' value="'+store[index+1]+'"><label>'+store[index+1]+'</label>'
-	    	 		//+'<input type="button" name="'+store[index]+' id="Winner" value="winner"/>';
-	    	 	display.append(games);
-	    	
-	    	 }
-	    	
-		});
-
-	}
+	// shuffle function takes the store array and ranmadizes it.
+	// then divedes that array into game groups of 2 players. 
 	
 
-	let nextRound =() => {
-		
-		store.filter((val,index) => {
-			
-	
-			document.getElementById("round").addEventListener("click", function() {
-		  		let cbChecked = document.querySelector("[value="+store[index]+"]:checked")
-		
-		  		if (cbChecked != null) {
-		    	winners.push(cbChecked.value)
-		    	}
-		    	console.log(winners)
-		  	});
-		 });
-		
-	}
 
-// shuffle function takes the store array and ranmadizes it.
-// then divedes that array into game groups of 2 players. 
-	
-	let shuffle = (store) =>{
+	let displayGames = (store) =>{
 		if (length > 0 ){
-			if (length % 2 === 0){
 			
+
+			if (length % 2 === 0){
+				
 				return shufflePlayers()
 			}
 
@@ -71,18 +36,67 @@ $(document).ready($ => {
 		} else document.getElementById('error').innerHTML="Please enter a players names";
 	}
 
+
+	let shufflePlayers =() =>{
+		
+		store.map((val,index) => {
+
+		    if (index % 2 === 0 || index === 0){
+		    	counter += 1;
+		    	
+		    	// Games push to the index page, game number, the players names and 
+		    	//radio buttons to record the win. 
+		    	let games ="<h3>"+"Game"+" "+counter+"</h3>"+
+	    	 		'<input type="radio" name='+store[index]+' value="'+store[index]+'"><label>'+store[index]+'</label>'+" "+ "v"+ " " +
+	    	 		'<input type="radio" name='+store[index]+' value="'+store[index+1]+'"><label>'+store[index+1]+'</label>'
+	    	 		//+'<input type="button" name="'+store[index]+' id="Winner" value="winner"/>';
+	    	 	display.append(games);
+	    	 }
+	    	
+		});
+		nextRound()
+
+	}
+	
+	let nextRound =() => {
+		let winners= [];
+	
+		store.map((val,index) => {
+			
+			document.getElementById("round").addEventListener("click", function() {
+
+		  		let cbChecked = document.querySelector("[name="+store[index]+"]:checked")
+		
+		  		if (cbChecked != null) {
+		    	 winners.push(cbChecked.value)
+		    	console.log(winners);
+		    displayGames(winners);
+
+		    	}	
+		    	
+		  	});
+
+		});
+		
+		
+		
+	}
+
 	addPlayer.on("click",(event) => {
 		 event.preventDefault();
 		if( input.val() !== ""){
 			store.push(input.val());
+			store.sort((a, b)=> 0.5 - Math.random());
 			length = store.length;
 			console.log(length);
+
 
 		 	let item = input.val();
 		 	let li = "<li>" + item + "</li>";
 		 	list.append(li);
 		 	input.val("");
 		 	console.log(store);
+
 		}
 		
 	});
@@ -90,17 +104,13 @@ $(document).ready($ => {
 
 	startGame.on("click",(event) => {
 		event.preventDefault();
-			return shuffle(store);
+			return displayGames(store);
 	});
 
 
-
-	
-
-
-
 	document.getElementById("display").innerHTML = store;
-	document.getElementById("round").innerHTML = winners;
+	
+	
 
 
 
