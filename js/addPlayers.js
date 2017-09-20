fccd78d95f3fa199c82adbbf36330a13fcd90370
Ben Naylor <bennaylor@Bens-MacBook-Air.local>
2017-09-20 09:30:55 +0100

    update

--- js/addPlayers.js
+++ js/addPlayers.js
@@ -1,12 +1,14 @@
 /* To do list 
 
 1. name validation,
-2. desiable radio buttons
-3. change to j query for radio selector
-4. One click buttons
+4. One click buttons, 
+next round buuton 
+5. refresh button
+
+7. name is not a number
+8. bye can not win 
+
 */
-
-
 
 $(document).ready($ => {
 	
@@ -19,100 +21,115 @@ $(document).ready($ => {
 	let error =$("#error");
 	let round= $("#round");
 	let test =$("#test");
-	counter =0;
+	roundCounter =0;
 	rounds = 0;
-
+	number = 0;
+	indexErray = [];
 
 	// whichround dicded which header should displayed depending on the number of players before 
 	//calling displayGames function
 
-	let whichRound= (arr) => {
-		rounds += 1
+	let displayGames = (arr) =>{
 		length = arr.length;
-		
 
-		if (length > 0 ){
+		if (length > 1 ){
+				
+			if (length % 2 === 0){
+				
+				return whichRound(arr)
+			}
 
-			if( rounds >  1 && length === 1){
+			if (length % 2 !== 0){
+		    	arr.push("bye");
 
-			let win = "<h1>"+"Winner"+"</h1>"+
+	    		return whichRound(arr)
+			}
+		    	  	
+		} 
+		else if( rounds >  1 && length === 1){
+				let win = "<h1>"+"Winner"+"</h1>"+
 				"<h4>"+arr[0]+"<h4>"
 				display.append(win);
-			}
-
-			if(length === 2){
-			let final ="<h1>"+"Final"+"</h1>"
-			display.append(final);
-
-			return displayGames(arr);
-			}
-
-			if(length === 4){
-			let semiFinal ="<h1>"+"SemiFinal"+"</h1>"
-			display.append(semiFinal);
-
-			return displayGames(arr);
-			}
-
-			if(length === 6 || length === 8){
-			let quaterFinal="<h1>"+"QuaterFinal"+"</h1>"
-			display.append(quaterFinal);
-
-			return displayGames(arr);
-			}
-
-			if(length > 8){
-			let rou ="<h1>"+"Round"+rounds+"</h1>"
-			display.append(rou);
-
-			return displayGames(arr);
-			}
-		} 
-		else{
+			} else {
 			let err = "<p>Please enter a players names</p>" 
 			return error.append(err);
 		}
 	}
 
+	let whichRound= (arr) => {
+		rounds += 1
+		length = arr.length;
+		
+			/*if( rounds >  1 && length === 1){
+
+			let win = "<h1>"+"Winner"+"</h1>"+
+				"<h4>"+arr[0]+"<h4>"
+				display.append(win);
+			}*/
+
+			if(length === 2){
+			let final ="<div>"+"<h1>"+"Final"+"</h1>"
+			display.append(final);
+
+			return shufflePlayers(arr);
+			}
+
+			if(length === 4){
+			let semiFinal ="<div>"+"<h1>"+"SemiFinal"+"</h1>"
+			display.append(semiFinal);
+
+			return shufflePlayers(arr);
+			}
+
+			if(length === 6 || length === 8){
+			let quaterFinal="<div>"+"<h1>"+"QuaterFinal"+"</h1>"
+			display.append(quaterFinal);
+
+			return shufflePlayers(arr);
+			}
+
+			if(length > 8){
+			let rou ="<div>"+"<h1>"+"Round"+rounds+"</h1>"
+			display.append(rou);
+
+			return shufflePlayers(arr);
+			}
+		
+	}
+
 	//displayGames works out if a buy is requird the calls shufflePlayers Function 
 
-	let displayGames = (arr) =>{
-		
-			
-		if (length % 2 === 0){
-			
-			return shufflePlayers(arr)
-		}
-
-		if (length % 2 !== 0){
-	    	arr.push("buy");
-
-    		return shufflePlayers(arr)
-		}
-		    	  	
-	}
 
 	//shufflePlayers assigs players to there groupings and displayes them on screen
 
 	let shufflePlayers =(array) =>{
 		
+
 		array.map((val,index) => {
 
 		    if (index % 2 === 0 || index === 0){
-		    	counter += 1;
+		    	roundCounter += 1;
+		    	number += 1;
+		    	indexErray.push(number);
+		    	console.log(indexErray);
 		    	
 		    	// Games push to the index page, game number, the players names and 
 		    	//radio buttons to record the win. 
-		    	let games = "<div>"+
-		    		"<h3>"+"Game"+" "+counter+"</h3>"+
-	    	 		'<input type="radio" name='+array[index]+' value="'+array[index]+'"><label>'+array[index]+'</label>'+" "+ "v"+ " " +
-	    	 		'<input type="radio" name='+array[index]+' value="'+array[index+1]+'"><label>'+array[index+1]+'</label>'
-	    	 			+"</div>"//'<input type="button" name="'+array[index]+' id="Winner" value="winner"/>';
+		    	let games = 
+
+		    	"<div class=form-group>"+"<form>"+
+		    		"<h3>"+"Game"+" "+roundCounter+"</h3>"+
+	    	 		'<input type="radio" id='+(number)+' name='+array[index]+' value="'+array[index]+'">'+" "+'<label>'+array[index]+'</label>'+" "+ "v"+ " " +
+	    	 		'<input type="radio" id='+(number += 1)+' name='+array[index]+' value="'+array[index+1]+'">'+" "+'<label>' +" " +array[index+1]+'</label>'
+	    	 			+"</form>"+"</div>"//<input type="button" name="'+array[index]+' id="Winner" value="winner"/>';
 	    	 	display.append(games);
+
+	    	 	indexErray.push(number);
+	    	 	console.log(indexErray);
 	    	}
 	    	
 		});
-		
+
 	}
 
 	//This function takes the players that won the round and add them 
@@ -124,28 +141,27 @@ $(document).ready($ => {
 	
 		arrs.map((val,index) => {
 
-
-		  		let winRadio = document.querySelector("[name="+arrs[index]+"]:checked")
+		  		let winRadio = document.querySelector("[name="+arrs[index]+"]:checked");
 		  	
 		
 		  		if (winRadio != null) {
-		    	 	 winners.push(winRadio.value)	
-		    	}
-		    //$("[name="+arrs[index]+"]").removeAttr("checked"); 	
-		
+		    	 	 winners.push(winRadio.value)
+		    	 	 console.log(winners)
+		    	} 
+		    	
 		});
 
-		//arrs.map((val,index) => {
+		indexErray.map((val) => {
 
-			//document.querySelector("[name = "+arrs[index]+ "].attr('checked',false");
-		  	
-		 //});
+			let rest = document.getElementById(val).checked = false;
+			let dis = document.getElementById(val).disabled = true;
+
+		});
 
 		arrs = 0;
 	 	arrs = winners;
-	  	return whichRound(arrs);
+	  	return displayGames(arrs);
 	  	console.log(arrs);
-	  	
 
 	}
 	//addPlayer click takes the inputs and display it a list and adds to an array, the array is the shuffled 
@@ -181,7 +197,7 @@ $(document).ready($ => {
 
 	startGame.on("click",(event) => {
 		event.preventDefault();
-			return whichRound(store);
+			return displayGames(store);
 	});
 
 	// on click of round the next round function is calleed
